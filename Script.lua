cafaf57592a1072f3b02c11283cc22687fff06af
zaklab-com <zaklabbusiness@gmail.com>
2024-06-28 18:42:23 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,6 @@
-print("Hello world!")
+local v__1 = Instance.new("ScreenGui");
+v__1.Parent = game.Players.LocalPlayer.PlayerGui;
+
+local v__2 = Instance.new("Frame");
+v__2.Size = UDim2.new(1,0,1,0);
+v__2.Parent = v__1
